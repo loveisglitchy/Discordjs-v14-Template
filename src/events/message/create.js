39e6c2ce8c503909c44de927ae4d3e452ad25070
loveisglitchy <72410854+loveisglitchy@users.noreply.github.com>
2022-09-01 09:02:51 -0500

    Update create.js

--- src/events/message/create.js
+++ src/events/message/create.js
@@ -13,8 +13,16 @@ module.exports = {
     )
       return null;
 
-    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>)\\s*`);
-    if (prefixRegex.test(message.content))
-      return message.reply(`I do not support legacy commands due to Discord limitations.`);
-  },
+       const prefixRegex = new RegExp(`^(<@!?${client.user.id}>)\\s*`);
+
+    let str = "";
+    if (message.guild.id !== process.env.DevGuild) {
+
+    str += "This is not my server. Please contact my developer."
+    }
+    if (prefixRegex.test(message.content)) {
+      str += `I do not support legacy commands due to Discord limitations.`
+    }
+    return message.reply(str)
+
 };
